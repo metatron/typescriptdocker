686873ef01ad5a1d5401d79011361f2d2a8652a4
merabi kamjin <merabi@merabi-no-MacBook-Pro.local>
2018-12-20 00:24:09 +0900

    streaming logic fixing.

--- src/py/nagesen.py
+++ src/py/nagesen.py
@@ -1,16 +1,20 @@
 import tweepy
 from twstreamlistener import TwStreamListener
 
+# 時間もあわせないと401エラーが返ってくる
 
 Consumer_key = 'UyAIOxvesKKY2UG15UTh6q4qA'
 Consumer_secret = '0YCYlkj7QHdmhgxlk71SBS6aAuq3GG33ipLbdFI5JBVnuDbzKV'
 Access_token = '3107466672-NIXkhTHiSsFce5e8QbePWnAx67nWpjgczyGRFka'
 Access_secret = '20sjAydS12nSv3L68cTYwKgmjnAlttJokMhuo1LFQ7UBA'
 
-auth = tweepy.OAuthHandler(Consumer_key, Consumer_secret)
-auth.set_access_token(Access_token, Access_secret)
-api = tweepy.API(auth, wait_on_rate_limit = True)
+try:
+    auth = tweepy.OAuthHandler(Consumer_key, Consumer_secret)
+    auth.set_access_token(Access_token, Access_secret)
+    api = tweepy.API(auth, wait_on_rate_limit = True)
 
-twStreamListener = TwStreamListener()
-twStream = tweepy.Stream(auth = api.auth, listener=twStreamListener)
-twStream.filter(track=['python'])
+    twStreamListener = TwStreamListener()
+    twStream = tweepy.Stream(auth = api.auth, listener=twStreamListener)
+    twStream.filter(track=['crypto'])
+except Exception as e:
+    print(e)
--- src/py/twstreamlistener.py
+++ src/py/twstreamlistener.py
@@ -2,7 +2,31 @@ import tweepy
 #override tweepy.StreamListener to add logic to on_status
 class TwStreamListener(tweepy.StreamListener):
     def __init__(self):
-        return
+        super(TwStreamListener, self).__init__()
+        #initializes the counter
+        self.counter = 0
 
     def on_status(self, status):
         print(status.text)
+        return True
+
+
+    def on_warning(self, notice):
+        print('WARNING:' + str(notice.message))
+        return
+
+    def on_exception(self, exception):
+        print('EXCEPTION:' + str(exception))
+        return
+
+    def on_error(self, status_code):
+        print('ERROR: ' + str(status_code))
+        return True
+
+    def on_connect(self):
+        print('CONNECTED')
+        return
+
+    def on_disconnect(self, notice):
+        print('DISCONNECTED:' + str(notice.code))
+        return
